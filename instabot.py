76188f5ee3e356a8c6bdb7eb47ac43afb8e46f83
Developer Gautam <39876486+developergautam07@users.noreply.github.com>
2020-07-23 16:13:33 +0530

    Create instabot.py

---
+++ instabot.py
@@ -0,0 +1,19 @@
+from instapy import InstaPy
+
+# Bot Login
+bot = InstaPy(username="your username", password="your password")
+bot.login()
+
+# Sending Likes To Posts
+hastags = ["python", "python3"]
+bot.like_by_tags(hastags, amount=4)
+
+# Following The Posts That Bot Likes
+bot.set_do_follow(True, percentage=50)
+
+# Commenting On Posts
+bot.set_do_comment(True, percentage=100)
+comments = ["Awesome Post", "Great Content, Do Check Page"]
+bot.set_comment(comments)
+
+bot.end()
